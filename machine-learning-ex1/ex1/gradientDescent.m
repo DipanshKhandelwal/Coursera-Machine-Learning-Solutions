5a8e4432dbd19ecb15e7d04f2eaf9e5d3f2e56c6
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2017-12-16 10:42:26 +0530

    Added gradient decent  algorithm

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -8,25 +8,20 @@ m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
-
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCost) and gradient here.
-    %
-
-
-
-
-
-
-
-    % ============================================================
-
-    % Save the cost J in every iteration    
-    J_history(iter) = computeCost(X, y, theta);
+  % ====================== YOUR CODE HERE ======================
+  % Instructions: Perform a single gradient step on the parameter vector
+  %               theta. 
+  %
+  % Hint: While debugging, it can be useful to print out the values
+  %       of the cost function (computeCost) and gradient here.
+  %
+  
+  theta = theta - (alpha/m) * (X' * (X * theta - y)); 
+  
+  % ============================================================
+  
+  % Save the cost J in every iteration    
+  J_history(iter) = computeCost(X, y, theta);
 
 end
 
