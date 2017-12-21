6535e0073be64ed4f5a14d77368cef2676fa4435
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2017-12-22 02:35:37 +0530

    Added Linear Regression Cost Function

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,16 +19,12 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+J = (1/(2*m))*sum((X*theta - y).^2) + (lambda/(2*m))*sum(theta(2:end).^2);
 
+grad_reg = (lambda/m).*theta;
+grad_reg(1) = 0;
 
-
-
-
-
-
-
-
-
+grad = ((1/m) .* X' * (X*theta - y)) + grad_reg;
 
 % =========================================================================
 
