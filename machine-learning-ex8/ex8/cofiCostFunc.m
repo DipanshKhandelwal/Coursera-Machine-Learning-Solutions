a21a36d09743fce043cc91eacdb3fa996809a665
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2017-12-27 01:43:24 +0530

    Added cofiCostFunc
    Complete ex-8

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,20 +40,13 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+j = sum(((X*Theta' - Y).^2)(R==1));
 
+J = (1/2)*sum(j + lambda.*(sum(sum(Theta.^2)) + sum(sum(X.^2))));
 
+X_grad = ((X*Theta' - Y).*R)*Theta + lambda.*X;
 
-
-
-
-
-
-
-
-
-
-
-
+Theta_grad = ((X*Theta' - Y).*R)'*X + lambda.*Theta;
 
 % =============================================================
 
